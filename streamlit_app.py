7a1bce2b3c855aa62b01a33fc6f9d83d5010d569
{dreamycat925} <{dreamycat925@gmail.com}>
2025-02-16 15:16:56 +0900

    change streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,5 +90,5 @@ if st.button("Compute All Thresholds"):
 
     # Display results
     st.write("### Computed Classification Thresholds")
-    threshold_df = pd.DataFrame(list(optimal_thresholds.items()), columns=["Cognitive Measure", "Threshold"])
-    st.dataframe(threshold_df)
+    threshold_df = pd.DataFrame(optimal_thresholds.values(), index=optimal_thresholds.keys()).T
+    st.table(threshold_df)
